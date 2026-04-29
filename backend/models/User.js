587c54906f3e1d790e19af197d248f3e8393df9f
gs27304 <gajendrasingh27304@gmail.com>
2026-04-29 15:57:08 +0530

    feat: implement User model with bcrypt hashing and link to Expenses

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+  }
+}, {
+  timestamps: true,
+});
+
+// Pre-save hook to hash password before saving to DB
+userSchema.pre('save', async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Helper method to compare entered password with hashed database password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
