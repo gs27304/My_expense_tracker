a1ecdbc1fd36a02faf1bbb158745715df013f95f
gs27304 <gajendrasingh27304@gmail.com>
2026-04-29 13:59:57 +0530

    feat: setup resilient MongoDB connection logic in config/db.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
 
 const app = express();
 
